26735027d5ed4f864122cc8b343af5e615a0a8b0
Kamunyamike <kamunyawanjohi@gmail.com>
2025-08-22 21:42:23 +0300

    Created Database models, included a superuser to my app. I also, included static files for styling and templates for including my html file.

--- blogging_app/blogging_app/asgi.py
+++ blogging_app/blogging_app/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for bogging_app project.
+ASGI config for blogging_app project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bogging_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogging_app.settings')
 
 application = get_asgi_application()
