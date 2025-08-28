c7fabbc852fb2aa2e37ad76982d5e1a932425073
Kamunyamike <kamunyawanjohi@gmail.com>
2025-08-28 23:19:00 +0300

    Created an update form so users can edit blog posts, by adding an UpdateView Class in my views.py file.

--- blogging_app/blog_app/views.py
+++ blogging_app/blog_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Post
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 
 # Create your views here.
 class BlogListView(ListView):
@@ -13,4 +13,8 @@ class BlogDetailView(DetailView):
 class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
-    fields = ['__all__']
+    fields = ['title', 'body', 'author']
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
